f6508eeb87ace6b7bbf45a4ef54657bbfa345f69
jsiskskskkskkskksksks-maker <jsiskskskkskkskksksks@gmail.com>
2026-02-19 22:00:28 -0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,14 +3,23 @@ package com.mouse.fixer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOnClickListener(v -> v.requestPointerCapture());
-        setContentView(layout);
+        
+        // Criamos um botão gigante. Quando clicar, o mouse trava no modo PC.
+        Button btn = new Button(this);
+        btn.setText("ATIVAR MOUSE PC");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.requestPointerCapture();
+            }
+        });
+        
+        setContentView(btn);
     }
 }
